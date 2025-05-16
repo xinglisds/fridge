74d377af50856a4b7f2853a28d496bb3072d06c0
Li Xing <56338900+xinglisds@users.noreply.github.com>
2025-05-15 23:45:49 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 # 集中管理API密钥
-OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY", os.environ.get("OPENAI_API_KEY", ""))
+OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY")
 
 # YouTube API 密钥
-YOUTUBE_API_KEY = st.secrets.get("YOUTUBE_API_KEY", os.environ.get("YOUTUBE_API_KEY", ""))
+YOUTUBE_API_KEY = st.secrets.get("YOUTUBE_API_KEY")
 
 # 其他可能的配置参数
 MAX_API_CALLS_PER_HOUR = 5
